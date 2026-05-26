7c5f5105b7a4cba8f5b9dc31d633f081e24ea035
twentyfourkarats <gb.defreitas.ca@gmail.com>
2026-05-26 16:58:09 -0600

    Add POS smart grid tile

---
+++ extensions/24k-barcode-scanner/src/Tile.jsx
@@ -0,0 +1,16 @@
+import {render} from 'preact';
+import '@shopify/ui-extensions/preact';
+
+export default async () => {
+  render(<Extension />, document.body);
+};
+
+function Extension() {
+  return (
+    <s-tile
+      heading="24K Barcode Scanner"
+      subheading="Scan alternate UPC"
+      onClick={() => shopify.action.presentModal()}
+    />
+  );
+}
