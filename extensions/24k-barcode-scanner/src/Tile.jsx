b19a11a58055b3e8f6e9f5a565947498ced5df33
twentyfourkarats <gb.defreitas.ca@gmail.com>
2026-05-26 17:22:52 -0600

    Remove JSX from POS tile

--- extensions/24k-barcode-scanner/src/Tile.jsx
+++ extensions/24k-barcode-scanner/src/Tile.jsx
@@ -1,17 +1,14 @@
-/** @jsx h */
 import {h, render} from 'preact';
 import '@shopify/ui-extensions/preact';
 
 export default async () => {
-  render(<Extension />, document.body);
+  render(h(Extension, null), document.body);
 };
 
 function Extension() {
-  return (
-    <s-tile
-      heading="24K Barcode Scanner"
-      subheading="Scan alternate UPC"
-      onClick={() => shopify.action.presentModal()}
-    />
-  );
+  return h('s-tile', {
+    heading: '24K Barcode Scanner',
+    subheading: 'Scan alternate UPC',
+    onClick: () => shopify.action.presentModal(),
+  });
 }
